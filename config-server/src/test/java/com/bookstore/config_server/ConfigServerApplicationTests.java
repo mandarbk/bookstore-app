524590dbabdaa48af1dd8f3e2bd4adfd51edb8da
mandarbk <uk.mandar@gmail.com>
2026-06-22 18:58:43 +0530

    Attempting to test dynamic changes

--- config-server/src/test/java/com/bookstore/config_server/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/bookstore/config_server/ConfigServerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.bookstore.config_server;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class ConfigServerApplicationTests {
 
 	@Test
