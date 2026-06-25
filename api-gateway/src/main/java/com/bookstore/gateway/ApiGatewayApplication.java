0776ac01da0ff1717586c14f2c6bbcdb03a4a727
mandarbk <uk.mandar@gmail.com>
2026-06-25 07:58:04 +0530

    Attempting to tigger delta builds. Only Api Gw and AuthServer

--- api-gateway/src/main/java/com/bookstore/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bookstore/gateway/ApiGatewayApplication.java
@@ -1,14 +1,17 @@
 package com.bookstore.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableRedisHttpSession(redisNamespace = "bookstore:session")
+@Slf4j
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+        log.info("API Gateway Application Started");
     }
 }
--- auth-server/src/main/java/com/bookstore/auth/AuthServerApplication.java
+++ auth-server/src/main/java/com/bookstore/auth/AuthServerApplication.java
@@ -1,12 +1,16 @@
 package com.bookstore.auth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})public class AuthServerApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@Slf4j
+public class AuthServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
+        log.info("Auth Server Application Started");
     }
 }
